60400d3a1eed299a15fe97209e571b6be9f97836
manav-1 <manav81101@gmail.com>
2021-10-10 17:17:25 +0530

    commit

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,8 +1,7 @@
 // Website related settings
 const settings = {
-  isSplash: true, // Change this to true if you want to use the splash screen.
-  useCustomCursor: false, // Change this to false if you want the good'ol cursor
-  googleTrackingID: "UA-174238252-2",
+  isSplash: true,
+  useCustomCursor: false,
 };
 
 //Home Page
